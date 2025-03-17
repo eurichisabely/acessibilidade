5c8fe315db6c0f5373c2647113503f66c470785b
eurichisabely <141160195+eurichisabely@users.noreply.github.com>
2025-03-17 10:38:47 -0300

    ok

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const aumentaFonteBotao = document.getElementById( 'aumenta-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+    l
